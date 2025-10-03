dd38d0cfb3f49a26eeba7cffb54f33668a9f871e
Paulo Henrique Reis <phdsreis@gmail.com>
2025-10-03 15:22:29 -0300

    feat: parametros

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,7 +9,18 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello world"))
+		product := r.URL.Query().Get("product")
+		id := r.URL.Query().Get("id")
+		if product != "" {
+			w.Write([]byte(product + id))
+		} else {
+			w.Write([]byte("teste"))
+		}
+	})
+
+	r.Get("/{productName}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "productName")
+		w.Write([]byte(param))
 	})
 	http.ListenAndServe(":3000", r)
 }
